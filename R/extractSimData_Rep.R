295f1cb19c93a868502685300815daae138a633b
ChloeRNater <chloe.nater@nina.no>
2023-05-25 14:39:01 +0200

    Add R_perF toggle in additional locations

--- R/extractSimData_Rep.R
+++ R/extractSimData_Rep.R
@@ -5,26 +5,39 @@
 #' @param DS.count array containing counts of adult and juvenile birds from 
 #' simulated distance sampling surveys. Dimensions: 1 = age class (1 = juveniles,
 #' 2 = adults), 2 = site/transect, 3 = year. 
-#'
+#' @param R_perF logical. If TRUE, treats recruitment rate as juvenile per adult female.
+#' If FALSE, treats recruitment rate as juvenile per adult (sum of both sexes).
+#' @param R_parent_drop0 logical. If TRUE, removes observations of juveniles without adults
+#' from recruitment data. If FALSE, sets 1 as the number of adults/adults females when none
+#' are observed. 
+#' 
 #' @return a list containing data on juveniles per adult from distance sampling
 #' surveys.
 #' @export
 #'
 #' @examples
 
-extractSimData_Rep <- function(Jmax, Tmax, DS.count){
+extractSimData_Rep <- function(Jmax, Tmax, DS.count, R_perF, R_parent_drop0){
   
   # Extract necessary data from DS counts
   sumR_obs <- c(DS.count[1,,])
-  sumAd_obs <- c(DS.count[2,,])
+  
+  if(R_perF){
+    sumAd_obs <- c(round(DS.count[2,,]*0.5))
+  }else{
+    sumAd_obs <- c(DS.count[2,,])
+  }
+  
   sumR_obs_year <- rep(1:Tmax, each = Jmax)
   
   # Discard entries with 0 adults present
-  drop.idx <- which(sumAd_obs == 0)
-  sumR_obs <- sumR_obs[-drop.idx]
-  sumAd_obs <- sumAd_obs[-drop.idx]
-  sumR_obs_year <- sumR_obs_year[-drop.idx]
-  
+  if(R_parent_drop){
+    drop.idx <- which(sumAd_obs == 0)
+    sumR_obs <- sumR_obs[-drop.idx]
+    sumAd_obs <- sumAd_obs[-drop.idx]
+    sumR_obs_year <- sumR_obs_year[-drop.idx]
+  }
+
   # Count observations
   N_sumR_obs <- length(sumR_obs)
   
