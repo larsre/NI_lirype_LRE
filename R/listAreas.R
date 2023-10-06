e1cdaa27096a669f965a9588df78b4378b5bd47b
Lars Rød-Eriksen <lars.rod-eriksen@nina.no>
2023-10-06 11:48:21 +0200

    Updated to match data sets per December 2022

--- R/listAreas.R
+++ R/listAreas.R
@@ -1,4 +1,9 @@
 #' List the areas that should be used in the analysis
+#' Areas are defined from 'Reporting levels', i.e. the spatial extent of the
+#' area used for local density estimation
+#' 
+#' The current list contains valid areas (reporting levels) for the three data
+#' sets openly available through GBIF (per Dec. 2022)
 #'
 #' @return a character vector with UTF-8 encoding. 
 #' @export
@@ -35,7 +40,7 @@ listAreas <- function(){
     "Øst Finnmark",
     "Øvre Numedal Fjellstyre",
     "Øystre Slidre Fjellstyre",
-    "Regionfelt Østlandet",
+    #"Regionfelt Østlandet", #exclude from analysis - very few observations/sporadic counting last years
     "Ringebu Fjellstyre",
     "Røyrvik Fjellstyre",
     "Selbu Fjellstyre",
@@ -48,7 +53,20 @@ listAreas <- function(){
     "Stjørdal Fjellstyre",
     "Vest Finnmark kyst",
     "Vestre Slidre Fjellstyre",
-    "Ytre Salten"
+    "Ytre Salten",
+    "Troms Ytre",
+    "Troms Midt",
+    "Troms Nord",
+    "Troms Sør",
+    #added 2021:
+    "Høylandet Fjellstyre",
+    "Øyer Fjellstyre",
+    #added 2022:
+    "Folldal Fjellstyre",
+    #added 2023:
+    #"Sunndal Fjellstyre",
+    #"Rendalen",
+    #"Statskog Bangdalen/Klinga utmarkslag Rype" #new name for 'Statskog og Klinga utm.' from 2023
   )
   
   Encoding(areas) <- "UTF-8"
