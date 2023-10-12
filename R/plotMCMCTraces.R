1f53a51d6d03d4711a42f7b8b7b589cfba4b514c
Lars Rød-Eriksen <lars.rod-eriksen@nina.no>
2023-10-12 11:22:18 +0200

    General code formatting.

--- R/plotMCMCTraces.R
+++ R/plotMCMCTraces.R
@@ -15,11 +15,23 @@
 plotMCMCTraces <- function(mcmc.out, fitRodentCov, VitalRates = TRUE, DetectParams = TRUE, PopSizes = TRUE, Densities = TRUE){
   
   ## Make parameter lists
-  mVR_params <- c("Mu.R", "h.Mu.R", "h.sigma.R", "sigmaT.R",
-                  "Mu.D1", "sigma.D", "ratio.JA1",
-                  "Mu.S", "Mu.S1", "h.Mu.S", "h.sigma.S")
-  if(fitRodentCov){
-    mVR_params <- c(mVR_params, "betaR.R", "h.Mu.betaR.R", "h.sigma.betaR.R")
+  mVR_params <- c(
+    "Mu.R",
+    "h.Mu.R",
+    "h.sigma.R",
+    "sigmaT.R",
+    "Mu.D1",
+    "sigma.D",
+    "ratio.JA1",
+    "Mu.S",
+    "Mu.S1",
+    "h.Mu.S",
+    "h.sigma.S"
+  )
+  
+  if (fitRodentCov) {
+    mVR_params <-
+      c(mVR_params, "betaR.R", "h.Mu.betaR.R", "h.sigma.betaR.R")
   }
   
   tVR_params <- c("R_year", "S")
@@ -37,75 +49,106 @@ plotMCMCTraces <- function(mcmc.out, fitRodentCov, VitalRates = TRUE, DetectPara
   
   ## Make plots and print to pdf
   plot.paths <- c()
-  
+  # Check if folder exists, if not create folder (NB! must use 'recursive' flag for first run)
+  ifelse(!dir.exists("Plots/MCMCTraces"), dir.create("Plots/MCMCTraces", recursive = T), FALSE)
+
   # Vital rates
-  if(VitalRates){
-    ifelse(!dir.exists("Plots/MCMCTraces"), dir.create("Plots/MCMCTraces"), FALSE) ## Check if folder exists, if not create folder
-    pdf("Plots/MCMCTraces/MCMCtrace_mVR.pdf", width = 8, height = 6)
+  if(VitalRates) {
+    pdf("Plots/MCMCTraces/MCMCtrace_mVR.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = mVR_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    pdf("Plots/MCMCTraces/MCMCtrace_tVR.pdf", width = 8, height = 6)
+    pdf("Plots/MCMCTraces/MCMCtrace_tVR.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = tVR_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    plot.paths <- c(plot.paths, "Plots/MCMCTraces/MCMCtrace_mVR.pdf", "Plots/MCMCTraces/MCMCtrace_tVR.pdf")
+    plot.paths <-
+      c(
+        plot.paths,
+        "Plots/MCMCTraces/MCMCtrace_mVR.pdf",
+        "Plots/MCMCTraces/MCMCtrace_tVR.pdf"
+      )
   }
   
   # Detection parameters
-  if(DetectParams){
-    pdf("Plots/MCMCTraces/MCMCtrace_mDet.pdf", width = 8, height = 6)
+  if(DetectParams) {
+    pdf("Plots/MCMCTraces/MCMCtrace_mDet.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = mDet_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    pdf("Plots/MCMCTraces/MCMCtrace_tDet.pdf", width = 8, height = 6)
+    pdf("Plots/MCMCTraces/MCMCtrace_tDet.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = tDet_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    plot.paths <- c(plot.paths, "Plots/MCMCTraces/MCMCtrace_mDet.pdf", "Plots/MCMCTraces/MCMCtrace_tDet.pdf")
+    plot.paths <-
+      c(
+        plot.paths,
+        "Plots/MCMCTraces/MCMCtrace_mDet.pdf",
+        "Plots/MCMCTraces/MCMCtrace_tDet.pdf"
+      )
   }
   
   # Population sizes
-  if(PopSizes){
-    pdf("Plots/MCMCTraces/MCMCtrace_Ntot.pdf", width = 8, height = 6)
+  if(PopSizes) {
+    pdf("Plots/MCMCTraces/MCMCtrace_Ntot.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = Ntot_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    pdf("Plots/MCMCTraces/MCMCtrace_N.pdf", width = 8, height = 6)
+    pdf("Plots/MCMCTraces/MCMCtrace_N.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = N_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    plot.paths <- c(plot.paths, "Plots/MCMCTraces/MCMCtrace_Ntot.pdf", "Plots/MCMCTraces/MCMCtrace_N.pdf")
+    plot.paths <-
+      c(
+        plot.paths,
+        "Plots/MCMCTraces/MCMCtrace_Ntot.pdf",
+        "Plots/MCMCTraces/MCMCtrace_N.pdf"
+      )
   }
   
   # Population densities
-  if(Densities){
-    pdf("Plots/MCMCTraces/MCMCtrace_Dens.pdf", width = 8, height = 6)
+  if(Densities) {
+    pdf("Plots/MCMCTraces/MCMCtrace_Dens.pdf",
+        width = 8,
+        height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = Dens_params,
                        ind = TRUE,
                        pdf = FALSE)
     dev.off()
     
-    plot.paths <- c(plot.paths, "Plots/MCMCTraces/MCMCtrace_Dens.pdf")
+    plot.paths <-
+      c(plot.paths, "Plots/MCMCTraces/MCMCtrace_Dens.pdf")
   }
   
   return(plot.paths)
