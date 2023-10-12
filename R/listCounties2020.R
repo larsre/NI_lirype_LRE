3ceeb410d87cf247f8cb053e2a075fcc6b399f85
Lars Rød-Eriksen <lars.rod-eriksen@nina.no>
2023-10-12 11:24:41 +0200

    Optional list of post-2020 counties.

---
+++ R/listCounties2020.R
@@ -0,0 +1,25 @@
+#' List counties to be included in the analysis - post-2020 county borders
+#'
+#' @return a character vector with UTF-8 encoding. 
+#' @export
+#'
+#' @examples
+
+listCounties2020 <- function(){
+  
+  counties <- c(
+    #"Agder",
+    #"Rogaland",
+    #"Vestland",
+    #"Viken",
+    #"Møre og Romsdal",
+    #"Vestfold og Telemark",
+    "Innlandet",
+    "Nordland",
+    "Troms og Finnmark",
+    "Trøndelag"
+  )
+  
+  Encoding(counties) <- "UTF-8"
+  return(counties)
+}
