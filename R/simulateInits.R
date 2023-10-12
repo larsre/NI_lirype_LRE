3c4cbb6d2fe547e930891638e7af5f144ad7a63a
Lars Rød-Eriksen <lars.rod-eriksen@nina.no>
2023-10-12 11:19:50 +0200

    Separated simulation of survival inits (included) from time variation in survival (optional). General code formatting.

--- R/simulateInits.R
+++ R/simulateInits.R
@@ -38,53 +38,55 @@ simulateInits <- function(nim.data, nim.constants, R_perF, shareRE, survVarT, fi
   # Missing covariate values #
   #--------------------------#
   
-  if(!is.null(nim.data$RodentOcc)){
-    RodentOcc <- nim.data$RodentOcc
-  }else{
-    RodentOcc <- matrix(0, nrow = N_areas, ncol = N_years)
+  if(fitRodentCov) {
+    if(!is.null(nim.data$RodentOcc)){
+      RodentOcc <- nim.data$RodentOcc
+    }else{
+      RodentOcc <- matrix(0, nrow = N_areas, ncol = N_years)
+    }
+    
+    if(NA %in% RodentOcc){
+      RodentOcc[which(is.na(RodentOcc))] <- runif(length(which(is.na(RodentOcc))), 0, 1)
+    }
+    
+    Inits_RodentOcc <- RodentOcc
+    Inits_RodentOcc[which(!is.na(nim.data$RodentOcc))] <- NA
   }
-  
-  if(NA %in% RodentOcc){
-    RodentOcc[which(is.na(RodentOcc))] <- runif(length(which(is.na(RodentOcc))), 0, 1)
-  }
-  
-  Inits_RodentOcc <- RodentOcc
-  Inits_RodentOcc[which(!is.na(nim.data$RodentOcc))] <- NA
-  
+
   
   # Vital rates #
   #-------------#
   
   ## Area-specific survival parameters
-  h.Mu.S <- runif(1, 0.3, 0.5) 
+  h.Mu.S <- runif(1, 0.3, 0.5)
   h.sigma.S <- runif(1, 0, 0.1)
   Mu.S1 <- runif(1, 0.6, 0.7)
   
-  mu.S <- EnvStats::rnormTrunc(N_areas, qlogis(h.Mu.S), sd = h.sigma.S, max = qlogis(Mu.S1))
-
-  sigmaT.S <- runif(1, 0, 0.05)
-  
+  mu.S <-
+    EnvStats::rnormTrunc(N_areas, qlogis(h.Mu.S), sd = h.sigma.S, max = qlogis(Mu.S1))
   Mu.S <- rep(NA, N_areas)
   S <-  matrix(NA, nrow = N_areas, ncol = N_years)
-  S1 <- S2 <- rep(NA, N_years)
   
-  epsT.S1.prop <- runif(1, 0.3, 0.8)
+  epsT.S <- matrix(0, nrow = N_areas, ncol = N_years)
+  #epsT.S <- matrix(rnorm(N_areas*N_years, 0, sigmaS.R), nrow = N_areas, ncol = N_years)
   
-  if(survVarT){
-    epsT.S <- matrix(0, nrow = N_areas, ncol = N_years)
-    #epsT.S <- matrix(rnorm(N_areas*N_years, 0, sigmaS.R), nrow = N_areas, ncol = N_years)
-    
-  }else{
-    epsT.S <- matrix(0, nrow = N_areas, ncol = N_years)
-  }
-  
-  for(x in 1:N_areas){
+  for (x in 1:N_areas) {
     Mu.S[x] <- plogis(mu.S[x])
-    S[x, 1:N_years] <- plogis(qlogis(Mu.S[x]) + epsT.S[x, 1:N_years])
+    S[x, 1:N_years] <-
+      plogis(qlogis(Mu.S[x]) + epsT.S[x, 1:N_years])
   }
-
-  S1[1:N_years] <- plogis(qlogis(Mu.S1) + epsT.S1.prop*epsT.S[nim.constants$SurvAreaIdx, 1:N_years])
-  S2[1:N_years] <- S[nim.constants$SurvAreaIdx, 1:N_years]/S1[1:N_years]
+  
+  if (survVarT) {
+    sigmaT.S <- runif(1, 0, 0.05)
+    S1 <- S2 <- rep(NA, N_years)
+    epsT.S1.prop <- runif(1, 0.3, 0.8)
+    
+    S1[1:N_years] <-
+      plogis(qlogis(Mu.S1) + epsT.S1.prop * epsT.S[nim.constants$SurvAreaIdx, 1:N_years])
+    S2[1:N_years] <-
+      S[nim.constants$SurvAreaIdx, 1:N_years] / S1[1:N_years]
+  }
+  
   
   ## Area-specific reproductive parameters
   h.Mu.R  <- runif(1, 1, 4)
@@ -107,21 +109,28 @@ simulateInits <- function(nim.data, nim.constants, R_perF, shareRE, survVarT, fi
   
   if(shareRE){
     epsT.R <- rep(0, N_years)
-    #epsT.R <- rnorm(N_year, 0, sigmaT.R)
+    #epsT.R <- rnorm(N_years, 0, sigmaT.R)
     
     for(x in 1:N_areas){
-      R_year[x, 1:N_years] <- exp(log(Mu.R[x]) + betaR.R[x]*RodentOcc[x, 1:N_years] + epsT.R[1:N_years])
+      if(fitRodentCov) {
+        R_year[x, 1:N_years] <- exp(log(Mu.R[x]) + betaR.R[x]*RodentOcc[x, 1:N_years] + epsT.R[1:N_years])
+      } else {
+        R_year[x, 1:N_years] <- exp(log(Mu.R[x]) + betaR.R[x] + epsT.R[1:N_years])
+      }
     }
   }else{
     epsT.R <- matrix(0, nrow = N_areas, ncol = N_years)
     #epsT.R <- matrix(rnorm(N_areas*N_years, 0, sigmaT.R), nrow = N_areas, ncol = N_years)
     
     for(x in 1:N_areas){
-      R_year[x, 1:N_years] <- exp(log(Mu.R[x]) + betaR.R[x]*RodentOcc[x, 1:N_years] + epsT.R[x, 1:N_years])
+      if(fitRodentCov) {
+        R_year[x, 1:N_years] <- exp(log(Mu.R[x]) + betaR.R[x]*RodentOcc[x, 1:N_years] + epsT.R[x, 1:N_years])
+      } else {
+        R_year[x, 1:N_years] <- exp(log(Mu.R[x]) + betaR.R[x] + epsT.R[x, 1:N_years])
+      }
     }
   }
 
-  
   
   # Detection parameters #
   #----------------------#
@@ -169,38 +178,44 @@ simulateInits <- function(nim.data, nim.constants, R_perF, shareRE, survVarT, fi
   sigma.D <- runif(N_areas, 0.05, 2)
   ratio.JA1 <- runif(N_areas, 0.2, 0.6)
   
-  N_exp <- Density <- array(0, dim = c(N_areas, N_ageC, max(N_sites), N_years))
+  N_exp <- Density <-
+    array(0, dim = c(N_areas, N_ageC, max(N_sites), N_years))
   
-  for(x in 1:N_areas){
-    
-    D_x_sum <- apply(nim.data$N_a_line_year[x,,,], c(2,3), sum) / (L[x,,]*W*2)
+  for (x in 1:N_areas) {
+    D_x_sum <-
+      apply(nim.data$N_a_line_year[x, , , ], c(2, 3), sum) / (L[x, , ] * W *
+                                                                2)
     D_data <- D_x_sum[which(!is.na(D_x_sum) & D_x_sum > 0)]
-    Mu.D1[x] <- runif(1, quantile(D_data, 0.25), quantile(D_data, 0.75))  
+    Mu.D1[x] <-
+      runif(1, quantile(D_data, 0.25), quantile(D_data, 0.75))
     
-    for(j in 1:N_sites[x]){
+    for (j in 1:N_sites[x]) {
+      Density[x, 1, j, 1] <- Mu.D1[x] * ratio.JA1[x]
+      Density[x, 2, j, 1] <- Mu.D1[x] * (1 - ratio.JA1[x])
       
-      Density[x, 1, j, 1] <- Mu.D1[x]*ratio.JA1[x]
-      Density[x, 2, j, 1] <- Mu.D1[x]*(1-ratio.JA1[x])
-      
-      N_exp[x, 1, j, 1] <- extraDistr::rtpois(1, lambda = Density[x, 1, j, 1]*L[x, j, 1]*W*2, a = 1)
-      N_exp[x, 2, j, 1] <- extraDistr::rtpois(1, lambda = Density[x, 2, j, 1]*L[x, j, 1]*W*2, a = 1)
+      N_exp[x, 1, j, 1] <-
+        extraDistr::rtpois(1, lambda = Density[x, 1, j, 1] * L[x, j, 1] * W * 2, a = 1)
+      N_exp[x, 2, j, 1] <-
+        extraDistr::rtpois(1, lambda = Density[x, 2, j, 1] * L[x, j, 1] * W * 2, a = 1)
     }
   }
 
   ## Population projection over time
-  for(x in 1:N_areas){
-    for(j in 1:N_sites[x]){
-      for(t in 2:N_years){
+  for (x in 1:N_areas) {
+    for (j in 1:N_sites[x]) {
+      for (t in 2:N_years) {
+        Density[x, 2, j, t] <-
+          sum(Density[x, 1:N_ageC, j, t - 1]) * S[x, t - 1] # Adults
         
-        Density[x, 2, j, t] <- sum(Density[x, 1:N_ageC, j, t-1])*S[x, t-1] # Adults
-        
-        if(R_perF){
-          Density[x, 1, j, t] <- (Density[x, 2, j, t]/2)*R_year[x, t] # Juveniles 
-        }else{
-          Density[x, 1, j, t] <- Density[x, 2, j, t]*R_year[x, t] # Juveniles
+        if (R_perF) {
+          Density[x, 1, j, t] <-
+            (Density[x, 2, j, t] / 2) * R_year[x, t] # Juveniles
+        } else{
+          Density[x, 1, j, t] <- Density[x, 2, j, t] * R_year[x, t] # Juveniles
         }
         
-        N_exp[x, 1:N_ageC, j, t] <- Density[x, 1:N_ageC, j, t]*L[x, j, t]*W*2
+        N_exp[x, 1:N_ageC, j, t] <-
+          Density[x, 1:N_ageC, j, t] * L[x, j, t] * W * 2
       }
     }
   }
@@ -208,9 +223,10 @@ simulateInits <- function(nim.data, nim.constants, R_perF, shareRE, survVarT, fi
   ## Area-specific population size and density
   N_tot_exp <- matrix(NA, nrow = N_areas, ncol = N_years)
   
-  for(x in 1:N_areas){
-    for (t in 1:N_years){
-      N_tot_exp[x, t] <- sum(N_exp[x, 1, 1:N_sites[x], t] + N_exp[x, 2, 1:N_sites[x], t])    ## Summing up expected number of birds in covered area; 
+  for (x in 1:N_areas) {
+    for (t in 1:N_years) {
+      N_tot_exp[x, t] <-
+        sum(N_exp[x, 1, 1:N_sites[x], t] + N_exp[x, 2, 1:N_sites[x], t])    ## Summing up expected number of birds in covered area;
     }
   }
   
@@ -242,17 +258,23 @@ simulateInits <- function(nim.data, nim.constants, R_perF, shareRE, survVarT, fi
     
     h.Mu.S = h.Mu.S,
     h.sigma.S = h.sigma.S,
-    mu.S = mu.S, Mu.S = Mu.S, 
-    sigmaT.S = sigmaT.S,
-    epsT.S = epsT.S,
     Mu.S1 = Mu.S1,
-    epsT.S1.prop = epsT.S1.prop,
-    S1 = S1, S2 = S2, S = S,
+    mu.S = mu.S,
+    Mu.S = Mu.S,
+    epsT.S = epsT.S,
+    S = S,
     
     Density = Density,
     N_exp = N_exp,
     N_tot_exp = N_tot_exp
   )
+  
+  if(survVarT){
+    InitVals$sigmaT.S <- sigmaT.S
+    InitVals$epsT.S1.prop <- epsT.S1.prop
+    InitVals$S1 <- S1
+    InitVals$S2 <- S2
+  }
   
   if(fitRodentCov){
     InitVals$h.Mu.betaR.R <- h.Mu.betaR.R
